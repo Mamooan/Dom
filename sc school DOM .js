a9650169b67987a21e052d625af3202d9cac54aa
Mamoon-Ahmad <mma282796@gmail.com>
2024-08-17 19:33:11 +0500

    first commit

---
+++ sc school DOM .js
@@ -0,0 +1,101 @@
+// var a=document.querySelector("div");
+// var b=document.querySelector("button");
+// var fu=0
+// b.addEventListener("click",()=>{
+//     if (fu==0) {
+//     a.style.backgroundColor="Yellow"
+//     a.style.boxShadow=" 0px 0px 20px 20px yellow"
+//     a.style.border="none" 
+//     b.innerText="Off"
+//     fu=1 
+//     }
+//     else{
+//     a.style.backgroundColor=" transparent"
+//     a.style.boxShadow="none"
+//     a.style.border=" 1px solid black" 
+//     b.innerText="On"
+//     fu=0  
+//     }
+// })
+
+
+
+/* ................... 5 project......................... */
+
+
+
+
+// var b=document.querySelector("button");
+// var a=0;
+// b.addEventListener('click',()=>{
+//     if (a==0) {
+//         b.innerText="cancel friend request"
+//         a=1
+//     }
+//     else{
+//         b.innerText="Add Friend"
+//         a=0
+//     }
+// })
+
+
+
+// var b=document.querySelector("button");
+// var a=document.querySelector(".aa");
+// b.addEventListener('click',()=>{
+//     a.classList.remove("aa");
+//     b.classList.add("aa");
+// })
+// a.addEventListener('click',()=>{
+//     b.classList.remove("aa");
+//     a.classList.add("aa");
+// })
+
+
+
+// var a=document.querySelector("div");
+// var b=document.querySelector("i");
+// a.addEventListener('dblclick',()=>{
+//     b.style.transform=" translate(-50%,-50%) scale(0.8)"
+//     b.style.opacity="0.7"
+//     setTimeout(() => {
+//     b.style.transform=" translate(-50%,-50%) scale(0.2)"
+//     }, 2000);
+//     setTimeout(() => {
+//     b.style.opacity="0"
+//     }, 2000);
+// })
+
+
+
+
+// var bb=document.querySelector("#ahmad");
+// var ss=document.querySelector(".a");
+// // var c=document.querySelector(".b");
+
+// // console.log(a,b,c);
+// bb.addEventListener("mousemove",(dat)=>{
+//     //   console.log(ss);
+//     ss.style.left = dat.x +'px'
+//       ss.style.top = dat.y +'px'
+// })
+
+
+
+// var q=document.querySelector("#q");
+// var q1=document.querySelector("#q1");
+// var q2=document.querySelector("#q2");
+// var q3=document.querySelector("#q3");
+// var q4=document.querySelector("#q4");
+
+// console.log(q1);
+// q.addEventListener("mousemove", (vv)=>{
+//     q1.style.left = vv.x +"px"
+//     q1.style.top = vv.y +"px"
+//     q2.style.left = vv.x +"px"
+//     q2.style.top = vv.y +"px"
+//     q3.style.left = vv.x +"px"
+//     q3.style.top = vv.y +"px"
+//     q4.style.left = vv.x +"px"
+//     q4.style.top = vv.y +"px"
+// })
